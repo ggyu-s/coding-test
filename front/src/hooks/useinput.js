832dd480388eaea4b4ae949d0d53b28bcb623821
ggyu <kcpooh0202@naver.com>
2021-05-10 16:05:17 +0900

    init

--- front/src/hooks/useinput.js
+++ front/src/hooks/useinput.js
@@ -1,5 +1,8 @@
 import { useCallback, useState } from "react";
 
+/**
+ * 입력창에서 공통으로 사용되기 때문에 커스텀하여 따로 설정하였습니다.
+ */
 function useInput(text) {
   const [value, setValue] = useState(text);
   const onChangeValue = useCallback((e) => {
